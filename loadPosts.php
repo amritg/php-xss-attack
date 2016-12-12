4447cd151931554ded6ebad102fd1d680f5fc6eb
Amrit Gautam <gautamamrit24@gmail.com>
2016-12-12 15:50:42 +0200

    Add TimeStamp column in DB and sort comments by newest first in comments board

--- loadPosts.php
+++ loadPosts.php
@@ -1,9 +1,9 @@
 <?php
     require("config.php");
-    $searchResult = mysqli_query($conn,"SELECT accountinformation.accountHolderFirstName, posts.message_body as post FROM accountinformation
-                                LEFT JOIN posts ON accountinformation.id = posts.posted_by");
+    $searchResult = mysqli_query($conn,"SELECT accountinformation.accountHolderFirstName, posts.created_at, posts.message_body as post FROM accountinformation
+                                INNER JOIN posts ON accountinformation.id = posts.posted_by ORDER BY posts.created_at DESC");
     echo "<table class='table table-striped'>";
     while($row = mysqli_fetch_assoc($searchResult)){
-        echo "<tr><td>$row[post] - $row[accountHolderFirstName]</td></tr>";
+        echo "<tr><td>$row[post] - $row[accountHolderFirstName] ($row[created_at])</td></tr>";
     }
 ?>
